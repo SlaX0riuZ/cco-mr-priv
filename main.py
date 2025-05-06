31ca2fc4e7af9c988ad026e322fb71537a7cc750
SlaX0riuZ <slaxoriuz@gmail.com>
2025-05-06 10:52:01 -0500

    ex.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from rollfunctions import *
 from cubelist import *
 from seriesconfig import *
+from matconfig import *
 
-material_rank_with_spincount("Dyes", 100000)
+material_rank_with_spincount(mat_display_name[1], 10000)
